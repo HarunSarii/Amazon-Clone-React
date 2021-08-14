258b533ed93c1f06beccb1f724b8eaeef9167f7f
HarunSarii <harunsari14@gmail.com>
2021-08-14 19:37:44 +0300

    add checkout page

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
-            <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+            <Link to='/'>
+                <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
+            </Link>
             <div className="header__search">
                 <input className='header_searchInput' type="text" name="" id="" />
                 <SearchIcon className='header__searchIcon' />
@@ -28,11 +31,12 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
 
                 </div>
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>0</span>
-                </div>
-
+                <Link to='/checkout'>
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>0</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
     return (
         <div className='product'>
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className='product__price'>
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    ****
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐</p>
+                    ))}
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="product-image" />
+            <img src={image} alt="product-image" />
             <button>Add to Basket</button>
         </div>
     )
