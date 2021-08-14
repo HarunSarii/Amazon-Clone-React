1b157f71d116df80fb44a2af19413538716fa609
HarunSarii <harunsari14@gmail.com>
2021-08-14 23:49:01 +0300

    add reducer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+    console.log('this is basket:', basket)
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
     return (
         <div className='product'>
             <div className="product__info">
@@ -17,7 +33,7 @@ function Product({ id, title, image, price, rating }) {
                 </div>
             </div>
             <img src={image} alt="product-image" />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
