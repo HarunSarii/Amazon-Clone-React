e3fe25b77739db8bd4e4a6ddca6191078d2c7172
HarunSarii <harunsari14@gmail.com>
2021-08-14 13:41:20 +0300

    initiate app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDk6knUJYd4pAdow7MC6c75bCBvEUjy7fo",
+    authDomain: "challenge-61a60.firebaseapp.com",
+    projectId: "challenge-61a60",
+    storageBucket: "challenge-61a60.appspot.com",
+    messagingSenderId: "93319267293",
+    appId: "1:93319267293:web:617d73e8ec301f6ba5ec34"
+};
