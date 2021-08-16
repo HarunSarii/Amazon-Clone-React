c545642fbba025ab8761b52984b8ac1c0d37325b
HarunSarii <harunsari14@gmail.com>
2021-08-16 03:30:00 +0300

    create components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import './App.css';
-import Header from './Header';
-import Home from './Home';
+import Header from './components/Header';
+import Home from './components/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Checkout from './Checkout';
-import Login from './Login';
-import { auth } from './firebase';
-import { useStateValue } from './StateProvider';
+import Checkout from './components/Checkout';
+import Login from './components/Login';
+import { auth } from './components/firebase';
+import { useStateValue } from './components/StateProvider';
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './StateProvider';
-import reducer, { initialState } from './reducer';
+import { StateProvider } from './components/StateProvider';
+import reducer, { initialState } from './components/reducer';
 
 ReactDOM.render(
   <React.StrictMode>
