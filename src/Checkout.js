bc904969a602f81e757129b71ee4420e6e48bf60
HarunSarii <harunsari14@gmail.com>
2021-08-16 03:06:23 +0300

    finished amazon clone

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,13 +5,14 @@ import Subtotal from './Subtotal'
 import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue()
+    const [{ basket, user }, dispatch] = useStateValue()
     return (
         <div className='checkout'>
             <div className="checkout__left">
                 <img className='checkout__ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                    alt="advertisement-image" />
+                    alt="advertisement" />
                 <div>
+                    <h3>Hello, {user ? user.email : 'Guest'}</h3>
                     <h2 className='checkout__title'>Your Shopping Basket</h2>
                     {basket.map(item => (
                         <CheckoutProduct
--- src/Product.js
+++ src/Product.js
@@ -32,7 +32,7 @@ function Product({ id, title, image, price, rating }) {
                     ))}
                 </div>
             </div>
-            <img src={image} alt="product-image" />
+            <img src={image} alt="product" />
             <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
